30f1c5697b751f9802bb825d17d495cfe8130ba7
Damian Oguche <damianguch@yahoo.com>
2023-10-28 09:44:26 +0100

    created gitignore file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ export const About = () => {
                     </p>
                 </div>
                 <div className="flex items-center justify-center">
-                    <img src={aboutImage} alt="" className="w-[400px] h-[400px] object-cover" />
+                    <img src={aboutImage} alt="" className="w-[300px] h-[300px] object-cover" />
                 </div>
             </div>
         </div>
