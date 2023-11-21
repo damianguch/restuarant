119b6540f82ce0515aad58c9012a3386cff67bd1
damian oguche <damianguch@yahoo.com>
2023-11-21 10:42:49 +0100

    Changes i the files

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use('/api/', stripeRouter);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
