ab906c24724a4c83cddca83381c89e777afcaf04
damian oguche <damianguch@yahoo.com>
2023-11-22 06:03:09 +0100

    modified index.js

--- server/index.js
+++ server/index.js
@@ -41,6 +41,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '0.0.0.0';
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
