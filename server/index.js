6448bcd0294da54680769fc06c0bba5da73528e2
damian oguche <damianguch@yahoo.com>
2023-11-22 06:50:39 +0100

    modified index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(
   })
 );
 
-//app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/', productRouter);
